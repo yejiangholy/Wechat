381551bd33f0437e01bdf2fa31831b8e3af46e8a
Carter Jiang <yejiangholy@gmail.com>
2016-04-01 00:21:12 -0500

    get login data

--- Server/src/WeChat/js/login.js
+++ Server/src/WeChat/js/login.js
@@ -1,13 +1,39 @@
-$(document).ready(function(){
-function submit(){
-var UserName = document.getElementById('UserName').innerHTML;
-var pwd = document.getElementById('pwd').innerHTML;
+function login(){
+  $( document ).ready(function() {
+    var Username = document.getElementById('UserName').value;
+    var pwd = document.getElementById('pwd').value;
+  alert(Username+"   "+pwd);
 
 
-var IsUser = true;
-if(IsUser)
-{
-  window.location.assign("http://www.apple.com")
+send login info to server && server tell correctness 
+});
+
+
+window.location = "http://www.w3schools.com";
 }
-}
-}
+    function sendXHR(verb, resource, body, cb) {
+      var xhr = new XMLHttpRequest();
+      xhr.open(verb, resource);
+
+
+      switch (typeof(body)) {
+        case 'undefined':
+          // No body to send.
+          xhr.send();
+          break;
+        case 'string':
+          // Tell the server we are sending text.
+          xhr.setRequestHeader("Content-Type", "text/plain;charset=UTF-8");
+          xhr.send(body);
+          break;
+        case 'object':
+          // Tell the server we are sending JSON.
+          xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+          // Convert body into a JSON string.
+          xhr.send(JSON.stringify(body));
+          break;
+        default:
+          throw new Error('Unknown body type: ' + typeof(body));
+      }
+
+    }
